1f1d346c9f0cd77291f20775de02d8cf77e259c7
VARUN NATRAJ V <147157561+Varun-Natraj@users.noreply.github.com>
2025-01-08 09:14:00 +0530

    Add files via upload

---
+++ HeiraricalInheritance.java
@@ -0,0 +1,32 @@
+class Animals {
+    void eat(){
+        System.out.println("The animal is eating now");
+    }
+}
+class Dog extends Animals{
+    void bark(){
+        System.out.println("The dog is barking");
+    }
+}
+class cat extends Animals{
+    void meow(){
+        System.out.println("The cat is meowing");
+    }
+}
+class gow extends Animals{
+    void shout(){
+        System.out.println("The gow is Shouting");
+    }
+}
+public class singleinheritence{
+    public static void main(String[] args)
+    {
+        Dog d1 = new Dog();
+        d1.eat();
+        d1.bark();
+        gow g1 = new gow();
+        g1.shout();
+        cat c1 = new cat();
+        c1.eat();
+    }
+}
